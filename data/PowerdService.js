f8c0b83b8409182c7432ed926d45e86279057944
Herrie <Github.com@herrie.org>
2015-03-15 10:20:55 +0100

    Fix detection of charging status

--- data/PowerdService.js
+++ data/PowerdService.js
@@ -27,7 +27,7 @@ enyo.kind({
 	components: [
 		{kind:enyo.PalmService, name:"batteryStatus", service:"palm://com.palm.bus/signal/", method:"addmatch", subscribe:true, onResponse:"handlePowerNotifications"},
 		{kind:"PalmService", name:"usbdockstatus", service:"palm://com.palm.bus/signal/", method:"addmatch", subscribe:true, onResponse:"handlePowerNotifications"},
-		{kind:"PalmService", name:"chargerStatusQuery", service:"palm://com.palm.power/com/palm/power/", method:"chargerStatusQuery"},
+		{kind:"PalmService", name:"chargerStatusQuery", service:"palm://com.palm.power/com/palm/power/", method:"chargerStatusQuery", onResponse:"handleChargerStatus"},
 		
 		{kind:"PalmService", name:"powerKeyPressed", service:"palm://com.palm.bus/signal/", method:"addmatch", subscribe:true, onResponse:"powerOffHandleNotifications"},
 	],
@@ -56,61 +56,7 @@ enyo.kind({
 		this.$.chargerStatusQuery.call();
 	},
 	
-	/*
- 	* Handle power and charging notifications
-	 */
-	handlePowerNotifications: function(inSender, inResponse) {
-		// Is the battery level provided?
-		if (inResponse.percent_ui != undefined) {
-			
-			this.batteryLevel = inResponse.percent_ui;	
-			
-			if (!this.isCharging) {
-					
-				//Show Banner Message if the Battery level is below 20%
-				if(this.batteryLevel <= 25) {
-					
-					var batPercentString = this.g11nPercent.format(this.batteryLevel);
-					var batteryalert = new enyo.g11n.Template($L("#{formattedBatteryLevel} battery remaining")).evaluate({formattedBatteryLevel: batPercentString});
-					
-					if(this.batteryLevel <= 5 && !this.batteryLevel5Shown) {
-						this.batteryLevel5Shown = true;
-						enyo.windows.openPopup("app/PowerdAlerts/powerdalerts.html", "LowBatteryAlert", {sound:"/usr/palm/sounds/battery_low.mp3", soundclass:"alerts"}, undefined, 150);
-					}
-					else if(this.batteryLevel > 5 && this.batteryLevel <= 10 && !this.batteryLevel10Shown) {
-						this.batteryLevel10Shown = true;
-						enyo.windows.addBannerMessage(batteryalert, "{}",'/usr/palm/applications/com.palm.systemui/images/battery-0.png', "notifications");					
-					}				
-					else if(this.batteryLevel > 10 && this.batteryLevel <= 20 && !this.batteryLevel20Shown) {
-						this.batteryLevel20Shown = true;	
-						enyo.windows.addBannerMessage(batteryalert, "{}",'/usr/palm/applications/com.palm.systemui/images/battery-1.png', "notifications");				
-					}
-				}
-					
-				if (this.batteryLevel < enyo.application.getSysUpdateService().getMinBatThresholdForUpdate()) {
-					enyo.application.getSysUpdateService().closeAllUpdateAlerts();
-				}				
-			}
-			
-			if(this.batteryLevel > 5 && this.batteryLevel <= 10) {				
-				this.batteryLevel5Shown = false;
-			}
-			else if(this.batteryLevel > 10 && this.batteryLevel <= 20) {
-				this.batteryLevel10Shown = false;
-			}
-			else if(this.batteryLevel > 20) {
-				this.batteryLevel20Shown = false;
-			}
-			
-			//Update the Software Update Alert
-	        if (this.batteryLevel >= enyo.application.getSysUpdateService().getMinBatThresholdForUpdate()) {
-				enyo.application.getSysUpdateService().updateBatteryLevelInAlert();
-			}
-			return;
-		}
-		
-		// Is this a charger notification?
-		
+	handleChargerStatus: function(inSender, inResponse) {
 		if(inResponse.DockConnected || inResponse.USBConnected) {
 			//This is the scenario where dock is detected but it's not placed it properly on the dock.
 			if(inResponse.DockConnected && !inResponse.DockPower) {
@@ -151,8 +97,6 @@ enyo.kind({
 					this.notChargingAlertTimer = setTimeout(enyo.bind(this, "showNotChargingAlert"), 2000);
 					this.notChargingAlertShown = true;
 				}
-					
-									
 			}
 		}
 		
@@ -170,6 +114,60 @@ enyo.kind({
 			if(enyo.windows.fetchWindow("NotChargingAlert"))
 				enyo.windows.fetchWindow("NotChargingAlert").close();
 			
+		}
+	},
+	
+	/*
+ 	* Handle power and charging notifications
+	 */
+	handlePowerNotifications: function(inSender, inResponse) {
+		// Is the battery level provided?
+		if (inResponse.percent_ui != undefined) {
+			
+			this.batteryLevel = inResponse.percent_ui;	
+			
+			if (!this.isCharging) {
+				//Show Banner Message if the Battery level is below 20%
+				if(this.batteryLevel <= 25) {
+					
+					var batPercentString = this.g11nPercent.format(this.batteryLevel);
+					var batteryalert = new enyo.g11n.Template($L("#{formattedBatteryLevel} battery remaining")).evaluate({formattedBatteryLevel: batPercentString});
+					
+					if(this.batteryLevel <= 5 && !this.batteryLevel5Shown) {
+						this.batteryLevel5Shown = true;
+						enyo.windows.openPopup("app/PowerdAlerts/powerdalerts.html", "LowBatteryAlert", {sound:"/usr/palm/sounds/battery_low.mp3", soundclass:"alerts"}, undefined, 150);
+					}
+					else if(this.batteryLevel > 5 && this.batteryLevel <= 10 && !this.batteryLevel10Shown) {
+						this.batteryLevel10Shown = true;
+						enyo.windows.addBannerMessage(batteryalert, "{}",'/usr/palm/applications/com.palm.systemui/images/battery-0.png', "notifications");					
+					}				
+					else if(this.batteryLevel > 10 && this.batteryLevel <= 20 && !this.batteryLevel20Shown) {
+						this.batteryLevel20Shown = true;	
+						enyo.windows.addBannerMessage(batteryalert, "{}",'/usr/palm/applications/com.palm.systemui/images/battery-1.png', "notifications");				
+					}
+				}
+					
+				if (this.batteryLevel < enyo.application.getSysUpdateService().getMinBatThresholdForUpdate()) {
+					enyo.application.getSysUpdateService().closeAllUpdateAlerts();
+				}				
+			}
+			
+			if(this.batteryLevel > 5 && this.batteryLevel <= 10) {				
+				this.batteryLevel5Shown = false;
+			}
+			else if(this.batteryLevel > 10 && this.batteryLevel <= 20) {
+				this.batteryLevel10Shown = false;
+			}
+			else if(this.batteryLevel > 20) {
+				console.log("Herrie 4 this.batteryLevel > 20"+this.batteryLevel);
+				this.batteryLevel20Shown = false;
+			}
+			
+			//Update the Software Update Alert
+	        if (this.batteryLevel >= enyo.application.getSysUpdateService().getMinBatThresholdForUpdate()) {
+				enyo.application.getSysUpdateService().updateBatteryLevelInAlert();
+			}
+			return;
 		}
 	},
 	
